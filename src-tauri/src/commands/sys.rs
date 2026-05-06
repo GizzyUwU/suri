4de62effa6c32a73a9288d60259ae37a579af21b
gizzy <nerop455@gmail.com>
2026-05-06 13:24:07 +0100

    feat: swap for whoami instead of users should fix windows?

--- src-tauri/src/commands/sys.rs
+++ src-tauri/src/commands/sys.rs
@@ -1,19 +1,19 @@
 use serde::Serialize;
-use users::{get_current_uid, get_user_by_uid};
+use whoami::{username};
 
 #[derive(Serialize)]
 pub struct SystemUser {
-    uid: u32,
+    // uid: u32,
     name: String,
-    primary_group: u32,
+    // primary_group: u32,
 }
 
 #[tauri::command]
 pub async fn sys_user() -> Result<SystemUser, String> {
-    let user = get_user_by_uid(get_current_uid()).ok_or("Failed to get current system user")?;
+    // let user = get_user_by_uid(get_current_uid()).ok_or("Failed to get current system user")?;
     Ok(SystemUser {
-        uid: user.uid(),
-        name: user.name().to_string_lossy().into_owned(),
-        primary_group: user.primary_group_id(),
+        // uid: user.uid(),
+        name: username().unwrap_or_else(|_| "<unknown>".to_string()),
+        // primary_group: user.primary_group_id(),
     })
 }
